77b8d1fb12cec98a62e5f8580301adaa25f60258
hemarubinni <rubivarsha25@gmail.com>
2023-07-28 16:42:45 +0530

    basic

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css'
+
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import React from 'react';
+
+import NavBar from './components/NavBar.jsx'
+function App() {
+
+  return (
+
+<><NavBar/></>
+
+
+
+  );
+
+}
+
+export default App;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,89 @@
+import React from 'react';
+import '../styles/Navbar.css'
+ const NavBar=()=>{
+  
+    return (
+<>
+<div className="root">
+ <div className="total">
+ <div className='header'>
+
+<div className="first">
+<p className='yas'>Hemarubinni G R</p>
+<p className='avai'>Now available</p>
+</div>
+
+<div className="second">
+
+<img className="a" src="/LogoBe.jpg" alt="a" />
+
+<img className='b' src="/dribbble-ball-mark.jpg" alt="b" />
+
+<img className='c' src="/LogoLinkedin.jpg" alt="c" />
+</div>
+       
+</div>
+</div>
+<div className="parent">
+     <div className="hi">
+     <p>👋Hi, I'm Hemarubinni</p>
+    
+</div>
+
+<div className="personal1">
+   <p>
+   I’m a Product Designer with a passion for participate in every stage of digital product, from discovery to delivery. Helping to focus the business goals on users.
+   </p>
+</div>
+
+
+<div className="personal2">
+<p>I’m looking for new opportunities
+Previously I worked at Multiplica Talent Chile</p>
+<p>Tirupur    
+ "mailto:hema@gmail.com";
+
+</p>
+</div>
+<div className="pro">
+  👇 Check some projects
+</div>
+</div>
+<div className="move">
+<div className="img1"></div>
+<img src="face-photo.jpg" alt="" />
+{/* <div className="s1"><img className="s1img"src="/fig.jpg" alt="" />
+</div> */}
+<div className="chart">🔀  Flow charts</div>
+<img src="/diamond.jpg" alt="" className="s2" />
+{/* <img src="/LogoBe.jpg" alt="" /> */}
+   <img className='s3' src="LogoBe.jpg" alt="" />
+<img src="/fig.jpg" alt="" className="s4" />
+ <div className="font">🔤 Fonts</div>  
+ <div className="ui">🎨 UI Kit</div>
+  <div className="user">🔍  User Research</div>
+ 
+ <div className="last">
+  <div className="div1"><img className="ss" src="/s1.jpg" alt=""/></div>
+  <div className="div2"><img className="ss1" src="/s2.jpg" alt=""/></div>
+  <div className="div3"><img className="ss2" src="/ss.jpg" alt=""/></div>
+  </div>
+  </div>
+  </div>
+  <div className="icon">🔔 Icons</div>
+  <div className="top">
+    
+<img className='a' src="/LogoBe.jpg" alt="a" />
+
+<img  className="b"src="/dribbble-ball-mark.jpg" alt="b" />
+
+<img className='c' src="/LogoLinkedin.jpg" alt="c" />
+  </div>
+</>
+    );
+    };
+    export default NavBar;
+
+
+
+
